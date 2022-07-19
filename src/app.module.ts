0a69bcc3840aaa000b5b8dc0a15aef97249bc25b
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-07-19 10:29:16 +0200

    Added student module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [AdminModule],
+  imports: [AdminModule, StudentModule],
   controllers: [AppController],
   providers: [AppService],
 })
