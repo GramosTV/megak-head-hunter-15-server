bfa5ae29da6aa5ef6fff6e98b7291d5c3ed67bf1
Mateusz <mateusz.masek@wp.pl>
2022-07-19 19:01:05 +0200

    Added Student entity.

--- src/student/entities/student.entity.ts
+++ src/student/entities/student.entity.ts
@@ -1,1 +1,34 @@
-export class Student {}
+enum expectedTypeWork {
+    Local,
+    ReadyToMove,
+    Remote,
+    Hybrid,
+    All,
+};
+
+enum expectedContractType {
+    EmploymentContract,
+    B2B,
+    CommisionContract,
+    NoPreferences,
+};
+
+export class Student {
+    email: string;
+    tel: number | null;
+    firstName: string;
+    lastName: string;
+    githubUsername: string;
+    portfolioUrls: string | null;
+    projectUrls: string;
+    bio: string | null;
+    expectedTypeWork: expectedTypeWork;
+    targetWorkCity: string | null;
+    expectedContractType: expectedContractType;
+    expectedSalary: null | number | string;
+    canTakeApprenticeship: boolean;
+    monthsOfCommercialExp: number;
+    education: string | null;
+    workExperience: string | null;
+    courses: string | null;
+};
