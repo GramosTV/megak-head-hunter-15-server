3ce15a8534ca5161177fec85d00d778881415cea
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-07-19 10:30:09 +0200

    Added HR module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
 import { StudentModule } from './student/student.module';
+import { HrModule } from './hr/hr.module';
 
 @Module({
-  imports: [AdminModule, StudentModule],
+  imports: [AdminModule, StudentModule, HrModule],
   controllers: [AppController],
   providers: [AppService],
 })
