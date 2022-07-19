b62a001db3452865feda672499e44bc00db98d93
Mateusz <mateusz.masek@wp.pl>
2022-07-19 20:53:00 +0200

    Added student entity.

--- src/student/entities/student.entity.ts
+++ src/student/entities/student.entity.ts
@@ -1,4 +1,4 @@
-import {BaseEntity, Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn} from "typeorm";
+import {BaseEntity, Column, Entity } from "typeorm";
 
 enum expectedTypeWork {
     Local,
@@ -16,22 +16,123 @@ enum expectedContractType {
 };
 
 @Entity()
-export class Student {
+export class Student extends BaseEntity {
+    @Column({
+        length: 320,
+        type: "text",
+        unique: true,
+        nullable: false,
+    })
     email: string;
+
+    @Column({
+        length: 15,
+        type: "tinyint",
+        nullable: true,
+    })
     tel: number | null;
+
+    @Column({
+        length: 256,
+        type: "tinytext",
+        nullable: false,
+    })
     firstName: string;
+
+    @Column({
+        length: 128,
+        type: "tinytext",
+        nullable: false,
+    })
     lastName: string;
+
+    @Column({
+        length: 39,
+        type: "tinytext",
+        nullable: false,
+    })
     githubUsername: string;
+
+    @Column({
+        length: 2000,
+        type: "text",
+        nullable: true,
+    })
     portfolioUrls: string | null;
+
+    @Column({
+        length: 2000,
+        type: "text",
+        nullable: false,
+    })
     projectUrls: string;
+
+    @Column({
+        length: 250,
+        type: "tinytext",
+        nullable: true,
+    })
     bio: string | null;
+
+    @Column({
+        length: 11,
+        type: "tinytext",
+        nullable: false,
+    })
     expectedTypeWork: expectedTypeWork;
+
+    @Column({
+        length: 189,
+        type: "tinytext",
+        nullable: true,
+    })
     targetWorkCity: string | null;
+
+    @Column({
+        length: 18,
+        type: "tinytext",
+    })
     expectedContractType: expectedContractType;
-    expectedSalary: null | number | string;
+
+    @Column({
+        length: 5,
+        type: "tinyint",
+        nullable: true,
+    })
+    expectedSalary: null | number;
+
+    @Column({
+        default: false,
+        type: "boolean",
+        nullable: false,
+    })
     canTakeApprenticeship: boolean;
+
+    @Column({
+        length: 2,
+        type: "tinyint",
+        nullable: false,
+    })
     monthsOfCommercialExp: number;
+
+    @Column({
+        length: 2000,
+        type: "longtext",
+        nullable: true,
+    })
     education: string | null;
+
+    @Column({
+        length: 2000,
+        type: "longtext",
+        nullable: true,
+    })
     workExperience: string | null;
+
+    @Column({
+        length: 2000,
+        type: "longtext",
+        nullable: true,
+    })
     courses: string | null;
 };
