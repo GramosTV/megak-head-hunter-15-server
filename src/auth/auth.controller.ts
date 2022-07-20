0164008d60fba15282e5f6c4352c5dc929237b18
Rafal <rgrajcar@gmail.com>
2022-07-20 19:04:33 +0200

    added methods in auth service for generating and adding actual valid token to user entity for login purposes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthLoginDto } from './dto/auth-login.dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { AuthLoginDto } from './dto/auth-login.dto';
+import { Response } from 'express';
+import { hashPwd } from '../utils/hash-pwd';
+import { v4 as uuid } from 'uuid';
+import { sign } from 'jsonwebtoken';
+import { JwtPayload } from './jwt.strategy';
+import { Student } from '../student/entities/student.entity';
 
 @Injectable()
 export class AuthService {
+  private createToken(currentTokenId: string): {
+    accessToken: string;
+    expiresIn: number;
+  } {
+    const payload: JwtPayload = { id: currentTokenId };
+    const expiresIn = 60 * 60 * 24;
+    const accessToken = sign(
+      payload,
+      'jkfdsufefnoisdfiosdfoweiofoifnodsifnosdnfsdfsdf',
+      { expiresIn },
+    );
+    return {
+      accessToken,
+      expiresIn,
+    };
+  }
 
+  private async generateToken(user: Student): Promise<string> {
+    let token;
+    let userWithThisToken = null;
+    do {
+      token = uuid();
+      userWithThisToken = await Student.findOneBy({ currentTokenId: token });
+    } while (!!userWithThisToken);
+    user.currentTokenId = token;
+    await user.save();
+
+    return token;
+  }
 }
