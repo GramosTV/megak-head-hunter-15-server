05463ce48583002c664918fe920ad2ab4a967a1e
Mateusz <mateusz.masek@wp.pl>
2022-07-20 22:08:11 +0200

    Added HR entity.

--- src/hr/entities/hr.entity.ts
+++ src/hr/entities/hr.entity.ts
@@ -1,1 +1,28 @@
-export class Hr {}
+import { BaseEntity, Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity()
+export class Hr extends BaseEntity {
+    @PrimaryColumn()
+    email: string;
+
+    @Column({
+        width: 384,
+        type: "text",
+        nullable: false,
+    })
+    fullName: string;
+
+    @Column({
+        width: 160,
+        type: "text",
+        nullable: false,
+    })
+    company: string;
+
+    @Column({
+        width: 3,
+        type: "tinyint",
+        nullable: false,
+    })
+    maxReservedStudents: number;
+}
