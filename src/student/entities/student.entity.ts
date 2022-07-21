b53a739c748675b8987dc2a56380f21d3b4805b6
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-07-21 10:57:39 +0200

    Added email column and changed primary column to uuid instead of email. Also added password column.

--- src/student/entities/student.entity.ts
+++ src/student/entities/student.entity.ts
@@ -1,120 +1,133 @@
-import { BaseEntity, Column, Entity, PrimaryColumn } from "typeorm";
-import { ExpectedContractType, ExpectedTypeWork } from "../interfaces/student";
-
+import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
+import { ExpectedContractType, ExpectedTypeWork } from '../interfaces/student';
 
 @Entity()
 export class Student extends BaseEntity {
-    @PrimaryColumn()
-    email: string;
+  @PrimaryColumn('uuid')
+  id: string;
 
-    @Column({
-        width: 15,
-        type: "tinyint",
-        nullable: true,
-    })
-    tel: number | null;
+  @Column({
+    width: 255,
+    type: 'text',
+    nullable: false,
+  })
+  email: string;
 
-    @Column({
-        width: 255,
-        type: "text",
-        nullable: false,
-    })
-    firstName: string;
+  @Column({
+    width: 255,
+    type: 'text',
+    nullable: false,
+  })
+  password: string;
 
-    @Column({
-        width: 128,
-        type: "text",
-        nullable: false,
-    })
-    lastName: string;
+  @Column({
+    width: 15,
+    type: 'tinyint',
+    nullable: true,
+  })
+  tel: number | null;
 
-    @Column({
-        width: 39,
-        type: "text",
-        nullable: false,
-    })
-    githubUsername: string;
+  @Column({
+    width: 255,
+    type: 'text',
+    nullable: false,
+  })
+  firstName: string;
 
-    @Column({
-        width: 2000,
-        type: "text",
-        nullable: true,
-    })
-    portfolioUrls: string | null;
+  @Column({
+    width: 128,
+    type: 'text',
+    nullable: false,
+  })
+  lastName: string;
 
-    @Column({
-        width: 2000,
-        type: "text",
-        nullable: false,
-    })
-    projectUrls: string;
+  @Column({
+    width: 39,
+    type: 'text',
+    nullable: false,
+  })
+  githubUsername: string;
 
-    @Column({
-        width: 250,
-        type: "text",
-        nullable: true,
-    })
-    bio: string | null;
+  @Column({
+    width: 2000,
+    type: 'text',
+    nullable: true,
+  })
+  portfolioUrls: string | null;
 
-    @Column({
-        type: "enum",
-        enum: ExpectedTypeWork,
-        nullable: false,
-    })
-    expectedTypeWork: ExpectedTypeWork;
+  @Column({
+    width: 2000,
+    type: 'text',
+    nullable: false,
+  })
+  projectUrls: string;
 
-    @Column({
-        width: 189,
-        type: "text",
-        nullable: true,
-    })
-    targetWorkCity: string | null;
+  @Column({
+    width: 250,
+    type: 'text',
+    nullable: true,
+  })
+  bio: string | null;
 
-    @Column({
-        type: "enum",
-        enum: ExpectedContractType,
-    })
-    expectedContractType: ExpectedContractType;
+  @Column({
+    type: 'enum',
+    enum: ExpectedTypeWork,
+    nullable: false,
+  })
+  expectedTypeWork: ExpectedTypeWork;
 
-    @Column({
-        width: 5,
-        type: "tinyint",
-        nullable: true,
-    })
-    expectedSalary: null | number;
+  @Column({
+    width: 189,
+    type: 'text',
+    nullable: true,
+  })
+  targetWorkCity: string | null;
 
-    @Column({
-        default: false,
-        type: "boolean",
-        nullable: false,
-    })
-    canTakeApprenticeship: boolean;
+  @Column({
+    type: 'enum',
+    enum: ExpectedContractType,
+  })
+  expectedContractType: ExpectedContractType;
 
-    @Column({
-        width: 2,
-        type: "tinyint",
-        nullable: false,
-    })
-    monthsOfCommercialExp: number;
+  @Column({
+    width: 5,
+    type: 'tinyint',
+    nullable: true,
+  })
+  expectedSalary: null | number;
 
-    @Column({
-        width: 2000,
-        type: "text",
-        nullable: true,
-    })
-    education: string | null;
+  @Column({
+    default: false,
+    type: 'boolean',
+    nullable: false,
+  })
+  canTakeApprenticeship: boolean;
 
-    @Column({
-        width: 2000,
-        type: "text",
-        nullable: true,
-    })
-    workExperience: string | null;
+  @Column({
+    width: 2,
+    type: 'tinyint',
+    nullable: false,
+  })
+  monthsOfCommercialExp: number;
 
-    @Column({
-        width: 2000,
-        type: "text",
-        nullable: true,
-    })
-    courses: string | null;
-};
+  @Column({
+    width: 2000,
+    type: 'text',
+    nullable: true,
+  })
+  education: string | null;
+
+  @Column({
+    width: 2000,
+    type: 'text',
+    nullable: true,
+  })
+  workExperience: string | null;
+
+  @Column({
+    width: 2000,
+    type: 'text',
+    nullable: true,
+  })
+  courses: string | null;
+}
