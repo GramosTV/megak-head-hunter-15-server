58db44f125677d4e75cf5d12adf352a37c8c7997
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-07-21 10:55:19 +0200

    Added admin entity

--- src/admin/entities/admin.entity.ts
+++ src/admin/entities/admin.entity.ts
@@ -1,1 +1,35 @@
-export class Admin {}
+import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class Admin extends BaseEntity {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column({
+    width: 255,
+    type: 'text',
+    nullable: false,
+  })
+  email: string;
+
+  @Column({
+    width: 255,
+    type: 'text',
+    nullable: false,
+  })
+  password: string;
+
+  @Column({
+    width: 255,
+    type: 'text',
+    nullable: false,
+  })
+  firstName: string;
+
+  @Column({
+    width: 128,
+    type: 'text',
+    nullable: false,
+  })
+  lastName: string;
+}
