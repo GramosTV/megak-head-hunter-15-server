0e6295b0b6538cea2ced0431f4954850531b4572
Rafal <rgrajcar@gmail.com>
2022-07-26 17:13:35 +0200

    added checkIfEmailIsUnique method

--- src/hr/hr.service.ts
+++ src/hr/hr.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { AddHrDto } from './dto/add-hr.dto';
+import { AddHrResponse } from '../student/interfaces/add-hr';
+import { User } from 'src/student/entities/user.entity';
 
 @Injectable()
 export class HrService {
-  create(createHrDto: AddHrDto) {
-    return 'This action adds a new hr';
+  async checkIfEmailIsUnique(email: string): Promise<boolean> {
+    const user = await User.findOne({
+      where: {
+        email: email,
+      },
+    });
+    return !!user;
+  }
+
+  async addHr(newHr: AddHrDto): Promise<AddHrResponse> {
+    const { email, fullName, company, maxReservedStudents } = newHr;
   }
 }
