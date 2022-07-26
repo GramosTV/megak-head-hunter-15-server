b21114819886e15f88b223e11e515f70844cbeea
Rafal <rgrajcar@gmail.com>
2022-07-26 17:25:27 +0200

    changed add-hr response type to cover positive or negative response

--- src/student/interfaces/add-hr.ts
+++ src/student/interfaces/add-hr.ts
@@ -1,5 +1,8 @@
-export interface AddHrResponse {
+export interface AddHrSuccessResponse {
+  isSuccess: true;
   email: string;
   fullName: string;
   company: string;
 }
+
+export type AddHrResponse = AddHrSuccessResponse | { isSuccess: false };
