5357cbd8fe79f8511152accc50b91986e637f68d
Rafal <rgrajcar@gmail.com>
2022-07-26 18:48:16 +0200

    added authorization for hr add controller - only admin can add new hr

--- src/hr/hr.controller.ts
+++ src/hr/hr.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
 import { HrService } from './hr.service';
 import { AddHrDto } from './dto/add-hr.dto';
+import { Roles } from '../decorators/roles.decorator';
+import { Role } from '../student/interfaces/user';
+import { RoleGuard } from '../auth/role.guard';
 
 @Controller('hr')
 export class HrController {
   constructor(private readonly hrService: HrService) {}
 
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard('jwt'), RoleGuard)
   @Post()
   create(@Body() newHr: AddHrDto) {
     return this.hrService.addHr(newHr);
