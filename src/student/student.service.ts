e3e327b43c1d9b4ff495f36987a5d738d8d23396
Mikołaj Gramowski <gramosmiki@onet.pl>
2022-08-02 20:10:47 +0200

    Change password

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
-
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 @Injectable()
 export class StudentService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+  async changePassword(user: User, password: string) {
+    const pw = await bcrypt.hash(password, 10);
+    user.password = pw;
+    await User.save(user);
+  }
+
   create(createStudentDto: CreateStudentDto) {
     return 'This action adds a new student';
   }
