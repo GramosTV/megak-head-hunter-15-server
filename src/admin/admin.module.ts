d96b847db0ee4c126e2ed391e47ce17060ae5400
Mikołaj Gramowski <gramosmiki@onet.pl>
2022-08-04 09:32:40 +0200

    added imports

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
-import { StudentService } from '../student/student.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../student/entities/user.entity';
+import { MailModule } from 'src/mail/mail.module';
+import { StudentModule } from 'src/student/student.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), MailModule, StudentModule],
   controllers: [AdminController],
-  providers: [AdminService, StudentService],
+  providers: [AdminService],
 })
 export class AdminModule {}
