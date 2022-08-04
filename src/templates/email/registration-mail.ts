5ef8951c2f95d1f2de7b5f5c8d71a0a610106d84
Mikołaj Gramowski <gramosmiki@onet.pl>
2022-08-04 21:32:42 +0200

    handlebars fix

--- src/templates/email/registration-mail.ts
+++ src/templates/email/registration-mail.ts
@@ -4,7 +4,9 @@ export function registrationMailTemplate(url: string) {
   return `
     <h1>Welcome to MegaK Head Hunter - Registration link</h1> 
     <br /> 
-    <b>To register click <a href={{striptags(url)}}>here</a></b>
-    <p>This link will expire in {{process.env.REGISTRATION_LINK_EXP_TIME_IN_DAYS + 'days'}}</p>
+    <b>To register click <a href="${striptags(url)}">here</a></b>
+    <p>This link will expire in ${
+      process.env.REGISTRATION_LINK_EXP_TIME_IN_DAYS + 'days'
+    }</p>
     `;
 }
