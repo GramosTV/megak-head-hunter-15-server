9f69903f03187899534ebdaaea17c9b9a10a3762
Rafal <rgrajcar@gmail.com>
2022-08-07 22:19:13 +0200

    added resetStudentsStatus method with cron schedule to daily reset the student status to available while the reservedTo date has passed

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { User } from '../student/entities/user.entity';
+import { LessThan } from 'typeorm';
+import { Role, Status } from '../student/interfaces/user';
 
 @Injectable()
-export class CronService {}
+export class CronService {
+  @Cron('0 0 0 * * *', {
+    name: 'Reset students status',
+  })
+  async resetStudentsStatus() {
+    const currentDate = new Date();
+    const usersToStatusReset = await User.find({
+      relations: {
+        hr: true,
+      },
+      where: {
+        role: Role.STUDENT,
+        reservedTo: LessThan(currentDate),
+      },
+    });
+    if (!usersToStatusReset) return;
+    usersToStatusReset.forEach(async (user) => {
+      user.status = Status.AVAILABLE;
+      user.reservedTo = null;
+      user.hr = null;
+      await user.save();
+    });
+  }
+}
