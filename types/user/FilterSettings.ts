f996f9d92ae1665bd3a9ee0152b48940432092dc
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-08-10 10:44:20 +0200

    Filter settings

---
+++ types/user/FilterSettings.ts
@@ -0,0 +1,16 @@
+import { BoolValues, ExpectedContractType, ExpectedTypeWork } from 'types';
+
+export interface FilterSettings {
+  fistName: string | null;
+  lastName: string | null;
+  courseCompletion: number | null;
+  courseEngagement: number | null;
+  projectDegree: number | null;
+  teamProjectDegree: number | null;
+  expectedTypeWork: ExpectedTypeWork | null;
+  expectedContractType: ExpectedContractType | null;
+  minNetSalary: number | null;
+  maxNetSalary: number | null;
+  canTakeApprenticeship: BoolValues | null;
+  monthsOfCommercialExp: number | null;
+}
