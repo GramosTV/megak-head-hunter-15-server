e0519b1a06d9f01ec6f9d05d9c4c72f902b3c654
Rafal <rgrajcar@gmail.com>
2022-08-11 20:36:00 +0200

    refactored the crone deleting expired call method to not remove the call reserved to the same day as the delete day

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import { LessThan } from 'typeorm';
 import { HrToStudent } from '../student/entities/hr-to-student.entity';
 
 @Injectable()
 export class CronService {
-  @Cron('0 0 0 * * *', {
+  @Cron(CronExpression.EVERY_10_SECONDS, {
+    //'0 0 0 * * *'
     name: 'Reset students status',
   })
   async resetStudentsStatus() {
-    const currentDate = new Date();
+    const currentDate = new Date(new Date().getTime() - 24 * 60 * 60 * 1000);
+    console.log(currentDate);
     await HrToStudent.delete({
       reservedTo: LessThan(currentDate),
     });
