32bd40eb578490ce7317db152af2427f7fffc753
Rafal <rgrajcar@gmail.com>
2022-08-11 20:40:47 +0200

    added the crone original schedule, cleaned up the service

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -1,17 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
+import { Cron } from '@nestjs/schedule';
 import { LessThan } from 'typeorm';
 import { HrToStudent } from '../student/entities/hr-to-student.entity';
 
 @Injectable()
 export class CronService {
-  @Cron(CronExpression.EVERY_10_SECONDS, {
-    //'0 0 0 * * *'
+  @Cron('0 0 0 * * *', {
     name: 'Reset students status',
   })
   async resetStudentsStatus() {
     const currentDate = new Date(new Date().getTime() - 24 * 60 * 60 * 1000);
-    console.log(currentDate);
     await HrToStudent.delete({
       reservedTo: LessThan(currentDate),
     });
