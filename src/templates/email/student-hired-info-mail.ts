f93cb245e45942dc2a44cbd0c9249a0845f4f707
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-08-17 09:55:59 +0200

    Protected from XSS

--- src/templates/email/student-hired-info-mail.ts
+++ src/templates/email/student-hired-info-mail.ts
@@ -1,9 +1,18 @@
 import { User } from '../../student/entities/user.entity';
+import * as striptags from 'striptags';
 
 export function hireInformationMailTemplate(hr: User, student: User) {
   return `
-    <h1>${student.firstName} ${student.lastName} został właśnie zatrudniony!</h1> 
+    <h1>${striptags(student.firstName)} ${striptags(
+    student.lastName,
+  )} został właśnie zatrudniony!</h1> 
     <br /> 
-    <p>${student.firstName} ${student.lastName} (id: ${student.id}, email: ${student.email}) został właśnie zatrudniony przez ${hr.fullName} z ${hr.company} (id: ${hr.id})!</p>
+    <p>${striptags(student.firstName)} ${striptags(
+    student.lastName,
+  )} (id: ${striptags(student.id)}, email: ${striptags(
+    student.email
+  )}) został właśnie zatrudniony przez ${striptags(hr.fullName)} z ${striptags(
+    hr.company
+  )} (id: ${striptags(hr.id)})!</p>
     `;
 }
